2bb0642a40316efbe8485cc36e19677d29a73197
Michael Pullin <michaelpullin75@ou.edu>
2026-05-27 21:24:19 -0500

    Week 1 Wednesday: 4-bit Counter with display output

--- week1/counter_4bit_tb.sv
+++ week1/counter_4bit_tb.sv
@@ -12,7 +12,13 @@ module tb_counter;
     clk = 0;
     resetN = 0; #10;
     resetN = 1; #10;
-    repeat(16) #10 clk = ~clk;
+    
+    repeat (8) begin
+  		#10 clk = 1;  // Rising edge
+  		$display("Count: %0d", count);
+ 		 #10 clk = 0;  // Falling edge (no print)
+	end
+    
     $finish;
   end
 endmodule
